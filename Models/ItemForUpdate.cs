f2d11964e52c9d982dce0b0cbe7bb4a221398efe
shaynagar <shaynagar12@gmail.com>
2017-06-05 01:40:53 +0300

    5 June 17

---
+++ Models/ItemForUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ngClassifiedsAPI.Models
+{
+    public class ItemForUpdate
+    {
+        public ItemForUpdate()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Price must contain only numbers")]
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
